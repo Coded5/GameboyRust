3ee9a31bd13d814e79e3fce153fc133e810401f6
Nutdanai Sikkhakul <phoenixanimation24@gmail.com>
2025-06-22 14:25:18 +0700

    remove debug tui
    Remove Debugger TUI as it no longer work

--- src/gui/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod cpu_state;
-#[allow(unused_parens)]
-pub mod debugger;
-pub mod disassembler;
-pub mod gameboy_screen;
-pub mod tilemap_debug;
-pub mod widgets;
--- src/gui/widgets/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod instructions_widget;
-pub mod memory_widget;
-pub mod registers_widget;
-pub mod stacks_widget;
-pub mod text_input_popup;
